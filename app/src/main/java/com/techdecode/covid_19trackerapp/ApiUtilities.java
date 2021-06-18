1e7ce00b2940f312b4a709ebc3b616dcc35c18e1
Filzazarin <filzazarin2002@gmail.com>
2021-06-19 00:19:32 +0530

    Covid-19 Tracker App

---
+++ app/src/main/java/com/techdecode/covid_19trackerapp/ApiUtilities.java
@@ -0,0 +1,18 @@
+package com.techdecode.covid_19trackerapp;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiUtilities {
+    public static Retrofit retrofit=null;
+    public static ApiInterface getAPIInterface()
+    {
+        if(retrofit==null)
+        {
+            retrofit=new Retrofit.Builder().baseUrl(ApiInterface.BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
+
+
+        }
+        return retrofit.create(ApiInterface.class);
+    }
+}
